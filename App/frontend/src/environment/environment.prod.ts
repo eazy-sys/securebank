9f687c07e3b1e7ca5fe868e7485a45bc0f4e2567
eazy-sys <omekeifebuche1@gmail.com>
2025-06-26 12:49:31 +0100

    deleted more files

--- App/frontend/src/environment/environment.prod.ts
+++ App/frontend/src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8180/api',
-  tokenName :  "authToken",
-  origin:'http://localhost:4200' 
+  apiUrl: 'http://174.177.117.22:8081/api',
+  tokenName: 'authToken',
+  origin: 'http://172.166.92.213'
 };
--- App/frontend/src/environment/environment.ts
+++ App/frontend/src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:8180/api',
-    tokenName :  "authToken",
-    origin:'http://localhost:4200' 
-  };
+  production: false,
+  apiUrl: 'http://174.177.117.22:8081/api', // Use backend external IP and port
+  tokenName: 'authToken',
+  origin: 'http://172.166.92.213' // Frontend external IP (if needed)
+};
