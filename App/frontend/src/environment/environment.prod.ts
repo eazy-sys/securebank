4ff8454ef822d56d0b7bbe52b8a8829ac16335ee
eazy-sys <omekeifebuche1@gmail.com>
2025-06-26 18:25:49 +0100

    new

--- App/frontend/src/environment/environment.prod.ts
+++ App/frontend/src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://131.145.99.89:8081/api',
+  apiUrl: '/api',
   tokenName: 'authToken',
-  origin: 'http://172.166.92.213'
+  origin: 'http://securebank.local'
 };
