2856f540a11191fa193b49e1d1438791cb527607
eazy-sys <omekeifebuche1@gmail.com>
2025-06-26 13:54:06 +0100

    new push

--- App/frontend/src/environment/environment.prod.ts
+++ App/frontend/src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://174.177.117.22:8081/api',
+  apiUrl: 'http://backend:8081/api',
   tokenName: 'authToken',
   origin: 'http://172.166.92.213'
 };
--- App/frontend/src/environment/environment.ts
+++ App/frontend/src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://174.177.117.22:8081/api', // Use backend external IP and port
+  apiUrl: 'http://backend:8081/api', // Use backend external IP and port
   tokenName: 'authToken',
   origin: 'http://172.166.92.213' // Frontend external IP (if needed)
 };
